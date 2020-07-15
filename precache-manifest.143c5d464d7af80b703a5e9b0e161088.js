e9d645f8ff11f4f411da993deb189aa569824e58
homfarnam <MY_NAME@example.com>
2020-07-15 10:47:21 +0430

    Updates

---
+++ precache-manifest.143c5d464d7af80b703a5e9b0e161088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf679874893c3d26c3966cd9e187172",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "5cbe249bd0d89b5153ff",
+    "url": "/amazon-clone/static/css/main.4ec2ff2f.chunk.css"
+  },
+  {
+    "revision": "d6e4a6dd2b11cbaf0189",
+    "url": "/amazon-clone/static/js/2.6d3d79fc.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/amazon-clone/static/js/2.6d3d79fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cbe249bd0d89b5153ff",
+    "url": "/amazon-clone/static/js/main.636f87d2.chunk.js"
+  },
+  {
+    "revision": "e1ecdc36eb4d4da21497",
+    "url": "/amazon-clone/static/js/runtime-main.fc8c5de8.js"
+  }
+]);
